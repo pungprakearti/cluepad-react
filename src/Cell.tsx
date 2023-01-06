35bcca3b5e939dfe9d6f904f7299365ddc9e8e44
Andrew Pungprakearti <andrew@alephsf.com>
2023-01-05 16:36:53 -0800

    Add sass

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,11 +1,12 @@
 import React = require('react')
+import styles from './Cell.module.scss'
 
 type Props = {
   type: 'button' | 'left-trans' | 'left-title' | 'left-item'
 }
 
 const Cell: React.FC<Props> = ({ type = 'button' }) => {
-  return <div></div>
+  return <div className={styles.[`wrap__${type}`]}></div>
 }
 
 export default Cell
