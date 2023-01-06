02f45569682e0d9535f0a4fcf676efbc00e4adc6
Andrew Pungprakearti <andrew@alephsf.com>
2023-01-05 16:27:45 -0800

    Add classnames

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,4 +1,10 @@
-const Cell = (type = '') => {
+import React = require('react')
+
+type Props = {
+  type: 'button' | 'left-trans' | 'left-title' | 'left-item'
+}
+
+const Cell: React.FC<Props> = ({ type = 'button' }) => {
   return <div></div>
 }
 
